14a8dde627ff99b04f2ce924e1719ea71cbae293
Skip Perry <vperry@stanford.edu>
2026-05-11 16:39:21 -0700

    progress

---
+++ data/venues/bus-stop.js
@@ -0,0 +1,125 @@
+window.VENUES.push({
+  id: "bus-stop",
+  name: "Bus Stop",
+  address: "1901 Union St, San Francisco, CA",
+  neighborhood: "Cow Hollow",
+  lat: 37.79759417765931,
+  lng: -122.43071488312248,
+  tables: [
+    {
+      name: "Left table",
+      table_size: "8 ft",
+      make: "Valley",
+      cloth_color: "Green",
+      // images: [
+      //   {
+      //     src: "./images/bus-stop-2025-07-01.jpg",
+      //     alt: "Pool table at Bus Stop",
+      //   },
+      // ],
+      review: {
+        cost: {
+          score: 4,
+          label: "Average",
+          comment: "$2 per game",
+        },
+        table_condition: {
+          score: 3,
+          label: "Average",
+          // comment: "Unique furniture-style table, high slate"
+        },
+        cloth_condition: {
+          score: 3,
+          label: "Average",
+        },
+        rails: {
+          score: 2,
+          label: "Fair",
+          comment: "Rails bouncy, set up low"
+        },
+        lighting: {
+          score: 5,
+          label: "Excellent",
+        },
+        ball_condition: {
+          score: 3,
+          label: "Average",
+        },
+        cue_ball: {
+          score: 4,
+          label: "Magnet",
+        },
+        obstructions: {
+          score: 4,
+          label: "Minor obstructions",
+          comment: "Close to seating when busy",
+        },
+        rolloff: {
+          score: 5,
+          label: "Level table",
+        },
+      },
+      pockets: {
+        label: "Typical",
+      },
+    },
+    {
+      name: "Right table",
+      table_size: "8 ft",
+      make: "Valley",
+      cloth_color: "Green",
+      images: [
+        {
+          src: "./images/bus-stop-2025-03-01.jpg",
+          alt: "Pool table at Bus Stop",
+        },
+      ],
+      review: {
+        cost: {
+          score: 4,
+          label: "Average",
+          comment: "$2 per game",
+        },
+        table_condition: {
+          score: 3,
+          label: "Average",
+          // comment: "Unique furniture-style table, high slate"
+        },
+        cloth_condition: {
+          score: 3,
+          label: "Average",
+        },
+        rails: {
+          score: 2,
+          label: "Fair",
+          comment: "Rails bouncy, set up low"
+        },
+        lighting: {
+          score: 5,
+          label: "Excellent",
+        },
+        ball_condition: {
+          score: 3,
+          label: "Average",
+        },
+        cue_ball: {
+          score: 4,
+          label: "Magnet",
+        },
+        obstructions: {
+          score: 3,
+          label: "Moderate obstructions",
+          comment: "Close to bathroom and seating when busy",
+        },
+        rolloff: {
+          score: 5,
+          label: "Level table",
+        },
+      },
+      pockets: {
+        label: "Typical",
+      },
+    }
+  ],
+  notes: "Interesting area at rear of venue",
+});
